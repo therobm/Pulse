20210c8473de8d618ae957eb6d841cbac5636712
Rob M <rob@mccoder.com>
2026-06-01 10:34:46 -0400

    perfer local cache if recent

--- Data/DataRoute.cs
+++ Data/DataRoute.cs
@@ -1,6 +1,6 @@
-
 using Microsoft.Maui.ApplicationModel;
 using System;
+using System.Collections.Generic;
 
 namespace Thump.Data
 {
@@ -13,15 +13,35 @@ namespace Thump.Data
 	public class DataRoute
 	{
 		public static bool s_bCacheEnabled = true;
+		private static int s_cacheDuration = 30;
 
 		public eRouteCachingMethod m_cacheType = eRouteCachingMethod.NetworkAuthorative;
 		protected ThumpData m_dataProvider;
+
+
+		private Dictionary<string, DateTime> m_cacheLastUpdated = new Dictionary<string, DateTime>();
+		
 		public DataRoute(ThumpData dataProvider, eRouteCachingMethod cacheType)
 		{
 			m_dataProvider = dataProvider;
 			m_cacheType = cacheType;
 		}
-
+		protected bool IsCacheExpired(string id = "default")
+		{
+			if (m_cacheLastUpdated.TryGetValue(id, out DateTime lastUpdated))
+			{
+				return (DateTime.Now - lastUpdated).TotalMinutes > s_cacheDuration;
+			}
+			return true;
+		}
+		protected void SetCached(string id = "default")
+		{
+			m_cacheLastUpdated[id] = DateTime.Now;
+		}
+		protected void ClearCache(string id = "default")
+		{
+			m_cacheLastUpdated.Remove(id);
+		}
 		protected void QueryDB(Action dataFunction)
 		{
 			m_dataProvider.Cache.ExecuteSync(dataFunction);
@@ -49,6 +69,26 @@ namespace Thump.Data
 
 		public void GetData(Action<T> callback)
 		{
+			if (!IsCacheExpired())
+			{
+				QueryDB(() =>
+				{
+					T cached = m_queryDatabase();
+
+					if (!m_dataValidator(cached))
+					{
+						//clear the bad cache and re-run this flow
+						ClearCache();
+						GetData(callback);
+					}
+					else
+					{
+						MainThread.BeginInvokeOnMainThread(() => { callback(cached); });
+					}	
+				});
+				return;
+			}
+
 			if (!IsOnline())
 			{
                 QueryDB(() =>
@@ -71,7 +111,11 @@ namespace Thump.Data
                     {
                         if (s_bCacheEnabled)
 						{
-							QueryDB(() => { m_storeDatabase(retVal); });
+							QueryDB(() => 
+							{ 
+								m_storeDatabase(retVal);
+								SetCached();
+							});
                         }
                         MainThread.BeginInvokeOnMainThread(() =>
                         {
@@ -115,7 +159,11 @@ namespace Thump.Data
                             {
                                 if (m_dataValidator(netData))
                                 {
-                                    QueryDB(() => { m_storeDatabase(netData); });
+                                    QueryDB(() => 
+									{ 
+										m_storeDatabase(netData);
+										SetCached();
+									});
                                 }
                                 MainThread.BeginInvokeOnMainThread(() => { callback(netData); });
                             });
@@ -129,7 +177,11 @@ namespace Thump.Data
 					{
 						if (m_dataValidator(netData))
 						{   
-							QueryDB(() => { m_storeDatabase(netData); });
+							QueryDB(() => 
+							{ 
+								m_storeDatabase(netData);
+								SetCached();
+							});
 						}
                         MainThread.BeginInvokeOnMainThread(() => { callback(netData); });
 					});
@@ -155,6 +207,26 @@ namespace Thump.Data
 
 		public void GetData(string id, Action<T> callback)
 		{
+			if (!IsCacheExpired(id))
+			{
+				QueryDB(() =>
+				{
+					T cached = m_queryDatabase(id);
+
+					if (!m_dataValidator(cached))
+					{
+						//clear the bad cache and re-run this flow
+						ClearCache(id);
+						GetData(id, callback);
+					}
+					else
+					{
+						MainThread.BeginInvokeOnMainThread(() => { callback(cached); });
+					}
+				});
+				return;
+			}
+
 			if (!m_dataProvider.Pulse.IsOnline())
 			{
 				m_dataProvider.Cache.ExecuteSync(() =>
@@ -180,7 +252,11 @@ namespace Thump.Data
                     {
                         if (s_bCacheEnabled)
 						{ 
-							QueryDB(() => { m_storeDatabase(id, retVal); });
+							QueryDB(() => 
+							{ 
+								m_storeDatabase(id, retVal);
+								SetCached(id);
+							});
 						}
                         MainThread.BeginInvokeOnMainThread(() => 
 						{
@@ -223,8 +299,12 @@ namespace Thump.Data
                             {
                                 if (m_dataValidator(netData))
                                 {
-                                    QueryDB(() => { m_storeDatabase(id, netData); });
-                                }
+                                    QueryDB(() =>
+									{
+										m_storeDatabase(id, netData);
+										SetCached(id);
+									});
+								}
                                 MainThread.BeginInvokeOnMainThread(() => { callback(netData); });
                             });
                         }
@@ -237,8 +317,12 @@ namespace Thump.Data
                     {
                         if (m_dataValidator(netData))
                         {
-                            QueryDB(() => { m_storeDatabase(id, netData); });
-                        }
+                            QueryDB(() =>
+							{
+								m_storeDatabase(id, netData);
+								SetCached(id);
+							});
+						}
                         MainThread.BeginInvokeOnMainThread(() => { callback(netData); });
                     });
                 }
