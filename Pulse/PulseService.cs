ea96a4bccee92ff2fe93724db770e93097a738b6
Rob M <rob@mccoder.com>
2026-06-13 18:37:26 -0400

    kill legacy analytics db

--- Pulse/PulseService.cs
+++ Pulse/PulseService.cs
@@ -82,7 +82,6 @@ namespace Pulse
 
 		private PulseData m_pulseData;
 		private MusicManager m_musicManager;
-		private AnalyticsDB m_analyticsDB;
 		private DiagnosticsData m_diagnosticsData;
 		private PodcastManager m_podcastManager;
 		private AudiobookManager m_audiobookManager;
@@ -125,8 +124,6 @@ namespace Pulse
 			s_musicManager = m_musicManager;
 			m_musicManager.Run(config.MusicPath);
 
-			m_analyticsDB = new AnalyticsDB(config);
-
 			m_diagnosticsData = new DiagnosticsData(config);
 			m_diagnosticsData.Load();
 
@@ -136,7 +133,7 @@ namespace Pulse
 			m_audiobookManager = new AudiobookManager(config);
 			m_audiobookManager.Run();
 
-			m_pulseEndpoints = new PulseEndpoints(this, m_musicManager, m_analyticsDB, m_diagnosticsData, m_podcastManager, m_audiobookManager);
+			m_pulseEndpoints = new PulseEndpoints(this, m_musicManager, m_diagnosticsData, m_podcastManager, m_audiobookManager);
 			
 			m_authEndpoints = new AuthEndpoints(m_pulseData);
 
