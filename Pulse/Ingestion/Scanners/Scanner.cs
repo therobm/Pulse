00f05a06c7458b58930de35125aa126dd1d44c4d
Rob M <rob@mccoder.com>
2026-06-28 04:06:55 -0400

    log scanner msgs

--- Pulse/Ingestion/Scanners/Scanner.cs
+++ Pulse/Ingestion/Scanners/Scanner.cs
@@ -53,6 +53,7 @@ namespace PulseIngestion.Scanners
 		}
 		protected void RecordInfo(string message)
 		{
+			Console.WriteLine(message);
 			if (m_section != null)
 			{
 				m_section.Add(eReportLevel.Info, message);
@@ -60,6 +61,7 @@ namespace PulseIngestion.Scanners
 		}
 		protected void RecordWarning(string message)
 		{
+			Console.WriteLine(message);
 			if (m_section != null)
 			{
 				m_section.Add(eReportLevel.Warning, message);
@@ -67,6 +69,7 @@ namespace PulseIngestion.Scanners
 		}
 		protected void RecordError(string message)
 		{
+			Console.WriteLine(message);
 			if (m_section != null)
 			{
 				m_section.Add(eReportLevel.Error, message);
